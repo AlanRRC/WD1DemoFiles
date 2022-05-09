9608ac85e0f51ce3a189cfbe063e963e08876abf
Alan Simpson <asimpson@rrc.ca>
2022-05-09 09:48:04 -0500

    Mid term changes

--- 8 Forms and Javascript/formvalidation.js
+++ 8 Forms and Javascript/formvalidation.js
@@ -1,6 +1,6 @@
 /*
 	Survey Form Validation
-	June 9 2020
+	Today's Date
 	Alan Simpson
  */
 
@@ -58,31 +58,6 @@ function resetForm(e){
 	return false;
 }
 
-/*
- * Displays the error for an invalid form field.
- *
- * param formField A reference to the form field that caused a validation error.
- * param errorId   The id of the error element to display.
- * param errorFlag True (an error has already occured), False (no errors have occured thus far)
- */
-function showError(formField, errorId, errorFlag){
-	// Set the display style of the error element to diplay
-	document.getElementById(errorId).style.display = "block";
-	
-	// Determine if this is the first error
-	// If so, set focus to the text field
-	if ( !errorFlag )
-	{
-		// Set focus to the text field that caused the error
-		formField.focus();
-		
-		if ( formField.type == "text" )
-		{
-			// Select the text in the text field
-			formField.select();
-		}		
-	}
-}
 
 /*
  * Does all the error checking for the form.
@@ -125,16 +100,6 @@ function hideMajor(){
 	// Hide the text field input for both majors
 	document.getElementById("bitmajordiv").style.display = "none";
 	document.getElementById("bamajordiv").style.display = "none";
-}
-
-/*
- * Removes white space from a string value.
- *
- * return  A string with leading and trailing white-space removed.
- */
-function trim(str){
-	// Uses a regex to remove spaces from a string.
-	return str.replace(/^\s+|\s+$/g,"");
 }
 
 /*
